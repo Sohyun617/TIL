3f1c9c29021c547c68f1199ff5e1e2913db26229
Sohyun Bae <bsohyun8@naver.com>
2020-09-16 23:25:50 +0900

    200915

---
+++ 클라우드AI과정/08AI/fashion/mnist_fahion.py
@@ -0,0 +1,10 @@
+#mnist fashion
+1. test 데이터 중에서 첫번째 데이터를 이미지로 표시 -> 이미지 제출
+2. fashion-mnist_train.csv(60000개), fashion-mnist_test.csv(10000개) 
+- random forest classifier
+- gradient boosting classifier
+- mlp classifier
+- svc classifier
+- 이 중 최적의 알고리즘과 파라미터 학습 (gridsearchCV)
+- 정답률 확인
+- jupyter 코드 제출
